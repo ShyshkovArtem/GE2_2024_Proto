18d36d80c100293cb0a02f69cde4e0fe0fd306ae
Artem Shyshkov <artemshyshkov222@gmail.com>
2024-08-29 22:00:19 +0300

    Movement and Camera
    Added BananaMan pref as Player, made smooth camera attached to the player, added movement script and idle, walk, run animations

---
+++ GE2_2024_Proto/Assets/Scripts/TopDown_Movement.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDown_Movement : MonoBehaviour
+{
+    private InputHandler _input;
+
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float rotateSpeed;
+
+    [SerializeField] private Camera camera;
+
+    [SerializeField] private Animator animator;
+
+    
+
+    void Start()
+    {
+        _input = GetComponent<InputHandler>();
+
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+
+        //Move in the aim direction
+        var moveVector = MoveTowardTarget(targetVector);
+
+        //Rotate to the move direction
+        RotateTowardMove(moveVector);
+
+        //Animator
+        if (!Input.GetKey("left shift") && (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")))
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+        }
+        if (!Input.GetKey("w") && !Input.GetKey("a") && !Input.GetKey("s") && !Input.GetKey("d"))
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+        }
+        if (Input.GetKey("left shift") && (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")))
+        {
+            animator.SetBool("isRunning", true);
+        }
+    }
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = moveSpeed * Time.deltaTime;
+
+        targetVector = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y, 0) * targetVector;
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+        return targetVector;
+    }
+    private void RotateTowardMove(Vector3 moveVector)
+    {
+        if (moveVector.magnitude == 0) return;
+        var rotation = Quaternion.LookRotation(moveVector);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed);
+    }
+
+    
+}
